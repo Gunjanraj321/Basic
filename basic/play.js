6c5cc0a63fb5822cf7ddc51fcaf3f63043cba6de
Gunjan Kumar <gunjanraj321@gmail.com>
2023-10-31 09:07:37 +0530

    object&console

--- basic/play.js
+++ basic/play.js
@@ -1,9 +1,21 @@
-var name = 'gunjan'
-var age = 23;
-var hobbies = "writing code"
+// var name = 'gunjan'
+// var age = 23;
+// var hobbies = "writing code"
 
-summary = (userName,userAge,userHobby) => {
-    return `name of the user ${userName} , having age ${userAge} & hobbies are ${userHobby}`
+// summary = (userName,userAge,userHobby) => {
+//     return `name of the user ${userName} , having age ${userAge} & hobbies are ${userHobby}`
+// }
+
+// console.log(summary(name,age,hobbies))
+
+const person = {
+    name: 'gunjan',
+    age: 25,
+    hobbies: 'writing code',
+    
+    greet: function() {
+        console.log(`hi i am ${this.name} and my hobbies ar ${this.hobbies}`)
+    }
 }
 
-console.log(summary(name,age,hobbies))
+console.log(person.greet())

22ebb9dffb4cc273d2e8308ed544f625848dfef8
Gunjan Kumar <gunjanraj321@gmail.com>
2023-10-31 10:19:39 +0530

    array with some changes

--- basic/play.js
+++ basic/play.js
@@ -8,14 +8,22 @@
 
 // console.log(summary(name,age,hobbies))
 
-const person = {
-    name: 'gunjan',
-    age: 25,
-    hobbies: 'writing code',
+// const person = {
+//     name: 'gunjan',
+//     age: 25,
+//     hobbies: 'writing code',
     
-    greet: function() {
-        console.log(`hi i am ${this.name} and my hobbies ar ${this.hobbies}`)
-    }
-}
+//     greet: function() {
+//         console.log(`hi i am ${this.name} and my hobbies ar ${this.hobbies}`)
+//     }
+// }
 
-console.log(person.greet())
+// console.log(person.greet())
+
+const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']
+console.log(array.map(arr => {
+    if(arr===' '){
+        return 'empty string'
+    }
+    return arr;
+}))
